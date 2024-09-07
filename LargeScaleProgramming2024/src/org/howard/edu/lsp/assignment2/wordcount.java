19887e187331ddfced967a4714e84ae3d606ef9a
Anverly <114864811+AnverlyJ@users.noreply.github.com>
2024-09-07 13:23:02 -0400

    function to count tokens and their frequencies

---
+++ LargeScaleProgramming2024/src/org/howard/edu/lsp/assignment2/wordcount.java
@@ -0,0 +1,18 @@
+package org.howard.edu.lsp.assignment2;
+
+import java.util.StringTokenizer;
+
+public class wordcount{
+	public static void main(String args[]) {
+		//create HashMap
+		HashMap<String, Integer> wordCountMap = new HashMap<>();
+		
+		//input file
+		
+		
+		//create tokenizer function
+		StringTokenizer str_arr = new StringTokenizer(txt.toLowerCase());
+		
+	}
+	
+}
