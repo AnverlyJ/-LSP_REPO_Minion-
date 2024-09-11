248109da104d837cd686123cf339416bff4b5e2f
Anverly <114864811+AnverlyJ@users.noreply.github.com>
2024-09-11 01:11:53 -0400

    text file reference

--- LargeScaleProgramming2024/src/org/howard/edu/lsp/assignment2/wordcount.java
+++ LargeScaleProgramming2024/src/org/howard/edu/lsp/assignment2/wordcount.java
@@ -1,17 +1,55 @@
 package org.howard.edu.lsp.assignment2;
 
+// 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class wordcount{
 	public static void main(String args[]) {
+		
+		 // Specify the path to the input file HERE (input file Name)
+        String filePath = "words.txt"; 
+        
+        
 		//create HashMap
 		HashMap<String, Integer> wordCountMap = new HashMap<>();
 		
-		//input file
+		// input file
 		
 		
-		//create tokenizer function
-		StringTokenizer str_arr = new StringTokenizer(txt.toLowerCase());
+		// Reading file and processing using bufferedReader
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+		
+		//create tokenizer function and format string to lowercase
+		StringTokenizer str_arr = new StringTokenizer(line.toLowerCase());
+		
+		
+		//count the tokens and how frequent each is
+		while (str_arr.hasMoreTokens()) {
+			String word = str_arr.nextToken();
+			//Only count words of length 4 or higher(excluding words 3 or lower)
+			 if (word.length() >= 4) {
+	              wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+	          }
+		}
+		 
+            
+        }
+      //exception for error
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+		
+		//print token and value of 
+		for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
 		
 	}
 	
